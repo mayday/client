8e97ae43fe3d9c8baa78c0ce07ec1ebc10a3d49f
Jorge Niedbalski R <niedbalski@gmail.com>
2014-11-16 00:38:47 -0300

    [core] added "mayday show" to display the current configuration and files

--- core/commands/show.go
+++ core/commands/show.go
@@ -16,12 +16,12 @@ func (cmd *ShowCommand) Name() string {
 }
 
 func (cmd *ShowCommand) Description() string {
-	return "Get the current reports files for a case."
+	return "Show the current configuration and files for a specific case."
 }
 
 func (cmd *ShowCommand) DefineFlags(fs *flag.FlagSet) {
-	cmd.uuid = fs.String("uuid", "", "PGP KeyID to sign the new configuration")
-	cmd.token = fs.String("token", "", "PGP KeyID to sign the new configuration")
+	cmd.uuid = fs.String("uuid", "", "Case UUID")
+	cmd.token = fs.String("auth", "", "Case authentication token")
 	cmd.server = fs.String("server", core.DefaultAPIBaseURL, "Mayday server address")
 }
 

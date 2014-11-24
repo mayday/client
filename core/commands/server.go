65c9a9aa4b6255032f3428347970576941daf733
Jorge Niedbalski R <niedbalski@gmail.com>
2014-11-24 09:41:40 -0300

    [core] mayday server command added

---
+++ core/commands/server.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"flag"
+	"mayday/server"
+)
+
+type ServerCommand struct {
+	port    *int
+	bind    *string
+	storage *string
+}
+
+func (cmd *ServerCommand) Name() string {
+	return "server"
+}
+
+func (cmd *ServerCommand) Description() string {
+	return "Start a mayday server."
+}
+
+func (cmd *ServerCommand) DefineFlags(fs *flag.FlagSet) {
+	cmd.storage = fs.String("storage", "", "Storage path for case report files")
+	cmd.port = fs.Int("port", server.DefaultPort, "Port to bind the mayday server")
+	cmd.bind = fs.String("bind", "0.0.0.0", "Address to bind the mayday server")
+}
+
+func (cmd *ServerCommand) Run() {
+	server.Start(*cmd.bind, *cmd.port, *cmd.storage)
+}

faaf21719182647d7ccd59c4efb2a5e5216df781
Jorge Niedbalski R <niedbalski@gmail.com>
2014-11-25 00:48:21 -0300

    Added "mayday pull" command

--- core/commands/pull.go
+++ core/commands/pull.go
@@ -2,13 +2,20 @@ package commands
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"mayday/core"
+	"os"
+	"path"
 )
 
 type PullCommand struct {
 	token  *string
-	uuid   *string
+	id     *string
 	server *string
+	all    *bool
+	fileId *string
+	to     *string
 }
 
 func (cmd *PullCommand) Name() string {
@@ -20,11 +27,71 @@ func (cmd *PullCommand) Description() string {
 }
 
 func (cmd *PullCommand) DefineFlags(fs *flag.FlagSet) {
-	cmd.uuid = fs.String("uuid", "", "PGP KeyID to sign the new configuration")
+	cmd.id = fs.String("id", "", "Case ID to pull from")
+	cmd.all = fs.Bool("all", true, "Pull all files from the case")
+	cmd.fileId = fs.String("file-id", "", "File Id to retrieve")
+	cmd.to = fs.String("to", "", "Path to store the retrieved files")
 	cmd.token = fs.String("token", "", "PGP KeyID to sign the new configuration")
 	cmd.server = fs.String("server", core.DefaultAPIBaseURL, "Mayday server address")
 }
 
 func (cmd *PullCommand) Run() {
+	if *cmd.id == "" {
+		fmt.Println("Please specify a valid Case Id --id\n")
+		os.Exit(1)
+	}
+
+	mayday, err := core.NewClient(*cmd.server, *cmd.id, *cmd.token)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var files map[string]string
+
+	if *cmd.fileId != "" {
+		files, err = mayday.Pull(*cmd.fileId)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	} else if !*cmd.all {
+		fmt.Println("Please specify a --file-id or --all")
+		os.Exit(1)
+	} else {
+		files, err = mayday.PullAll()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	for name, content := range files {
+		filename := ""
+		if *cmd.to == "" {
+			base, err := core.GetDefaultDirectory()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			base = path.Join(base, "pull", *cmd.id)
+			if _, err := os.Stat(base); os.IsNotExist(err) {
+				err := os.MkdirAll(base, 0700)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+
+			filename = path.Join(base, name)
+		} else {
+			filename = path.Join(*cmd.to, name)
+		}
+
+		err := ioutil.WriteFile(filename, []byte(content), 0700)
+		if err != nil {
+			fmt.Println("Error writing file")
+		} else {
+			fmt.Printf("Pulled report on path: %s\n", filename)
+		}
+	}
+
 	// real stuff
 }
